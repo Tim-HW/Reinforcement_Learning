1786cd1e36f4c517a023095c2a1146b8d656db6c
Tim <hollow1232010@gmail.com>
2022-06-12 13:22:10 +0200

    advanced

--- gym-3-classical-Q-learning-continuous.py
+++ gym-3-classical-Q-learning-continuous.py
@@ -4,19 +4,44 @@ import matplotlib.pyplot as plt
 import gym
 
 
+
+
+NUM_BINS = 10
 # Number of episodes played
 EPOCHS = 20000
 # Learning Rate
 ALPHA  = 0.8
 # Discount Rate
 GAMMA  = 0.95
-# Greedy Epsilon term
-epsilon     = 1.0
-max_epsilon = 1.0
-min_epsilon = 0.01
-decay_rate  = 0.001
-# interval to see where we are
-log_interval = 1000
+
+
+
+
+
+def create_bins(num_bins_per_action=10):
+
+    bins_cart_position         = np.linspace( -4.8 , 4.8 ,num_bins_per_action)
+    bins_cart_velocity         = np.linspace(  -5  ,  5  ,num_bins_per_action)
+    bins_pole_angle            = np.linspace(-0.418,0.418,num_bins_per_action)
+    bins_pole_angular_velocity = np.linspace(  -5  ,  5  ,num_bins_per_action)
+
+    bins = np.array([bins_cart_position,bins_cart_velocity,bins_pole_angle,bins_pole_angular_velocity])
+
+    return bins
+
+
+
+def discretize_observation_function(observations,bins):
+    # init matrix
+    binned_observations = []
+    # for every values in a row of observation
+    for i,observation in enumerate(observations):
+        # discretize the value
+        discretized_observation = np.digitize(observation,bins[i])
+        # add it into matrix
+        binned_observations.append(discretized_observation)
+    # return the final binned values
+    return tuple(binned_observations)
 
 
 
@@ -26,8 +51,11 @@ env = gym.make('CartPole-v1')
 observation = env.reset()
 
 
-for episode in range(EPOCHS):
+Q_table_shape = (NUM_BINS,NUM_BINS,NUM_BINS,NUM_BINS,env.action_space.n)
+Q_table = np.zeros([Q_table_shape])
 
+"""
+for episode in range(EPOCHS):
 
     env.render()
     # ACTION
@@ -35,6 +63,5 @@ for episode in range(EPOCHS):
     # OBSERVATION
     observation,reward,done,info = env.step(action)
     
-
-
 env.close()
+"""
